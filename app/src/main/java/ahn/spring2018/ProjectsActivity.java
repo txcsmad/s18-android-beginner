2f6968fbf5f3ddff0d8f838dd729620ddf28f4a7
alinaqvi97 <alinaqvi1397@gmail.com>
2018-02-19 18:36:50 -0600

    Finish lesson 3

---
+++ app/src/main/java/ahn/spring2018/ProjectsActivity.java
@@ -0,0 +1,54 @@
+package ahn.spring2018;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ProjectsActivity extends AppCompatActivity {
+
+    ArrayList<String> projectNames;
+    ArrayAdapter<String> arrayAdapter;
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_projects);
+
+        projectNames = new ArrayList<>();
+        projectNames.add("Scarne's Dice");
+        projectNames.add("Schedule App");
+
+        listView = findViewById(R.id.projects_list);
+
+        arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item,
+                R.id.list_item, projectNames);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent;
+                switch(position) {
+                    case 0:
+                        intent = new Intent(ProjectsActivity.this, MainActivity.class);
+                        break;
+                    case 1:
+                        intent = new Intent(ProjectsActivity.this, ScheduleActivity.class);
+                        break;
+                    default:
+                        intent = new Intent(ProjectsActivity.this, MainActivity.class);
+                        break;
+                }
+
+                startActivity(intent);
+            }
+        });
+    }
+}
