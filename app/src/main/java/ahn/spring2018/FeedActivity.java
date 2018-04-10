b4e475393f8b80e0b916bd47994a7c42036cf1b2
Ali Naqvi <alinaqvi1397@gmail.com>
2018-04-10 18:23:04 -0500

    Finish lesson 9, firebase project

--- app/src/main/java/ahn/spring2018/FeedActivity.java
+++ app/src/main/java/ahn/spring2018/FeedActivity.java
@@ -14,17 +14,56 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class FeedActivity extends AppCompatActivity {
     private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("data");
     private ArrayList<String> urls, captions, user, timestamps;
     private FeedAdapter adapter;
+
+    private ValueEventListener eventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            refresh();
+            for (DataSnapshot timeData : dataSnapshot.getChildren()) {
+                timestamps.add(timeData.getKey());
+                for (DataSnapshot userData : timeData.getChildren()) {
+                    String value = userData.getValue(String.class);
+                    switch (userData.getKey()) {
+                        case "url":
+                            urls.add(value);
+                            break;
+                        case "caption":
+                            captions.add(value);
+                            break;
+                        default:
+                            user.add(value);
+                    }
+                }
+            }
+
+            Collections.reverse(urls);
+            Collections.reverse(user);
+            Collections.reverse(timestamps);
+            Collections.reverse(captions);
+            adapter.notifyDataSetChanged();
+
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +84,25 @@ public class FeedActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         adapter = new FeedAdapter(this, captions, urls, user, timestamps);
         recyclerView.setAdapter(adapter);
+    }
+
+    private void refresh() {
+        urls.clear();
+        timestamps.clear();
+        user.clear();
+        captions.clear();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        databaseReference.addValueEventListener(eventListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        databaseReference.removeEventListener(eventListener);
     }
 
     @Override
@@ -68,7 +126,7 @@ public class FeedActivity extends AppCompatActivity {
 
         public FeedHolder(View itemView) {
             super(itemView);
-            imageView = itemView.findViewById(R.id.upload_pic);
+            imageView = itemView.findViewById(R.id.image_preview);
             usernameTextview = itemView.findViewById(R.id.user_name);
             captionTextview = itemView.findViewById(R.id.caption_textview);
             timeTextview = itemView.findViewById(R.id.time_stamp);
@@ -85,6 +143,7 @@ public class FeedActivity extends AppCompatActivity {
             this.context = context;
             this.captions = captions;
             this.timestamps = timestamps;
+            this.urls = urls;
             this.users = users;
         }
 
@@ -96,11 +155,20 @@ public class FeedActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onBindViewHolder(FeedHolder holder, int position) {
+        public void onBindViewHolder(FeedHolder holder, final int position) {
             holder.timeTextview.setText(timestamps.get(position));
             holder.usernameTextview.setText(users.get(position));
             holder.captionTextview.setText(captions.get(position));
             Picasso.with(context).load(urls.get(position)).into(holder.imageView);
+
+            holder.imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(FeedActivity.this, FullscreenActivity.class);
+                    intent.putExtra("url", urls.get(position));
+                    startActivity(intent);
+                }
+            });
         }
 
         @Override
